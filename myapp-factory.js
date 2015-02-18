689a7e50f802a4ec430e5c703addaead43eec16e
rerdos <ricsi96@gmail.com>
2015-02-18 16:37:33 +0100

    Main page and factory were added

---
+++ myapp-factory.js
@@ -0,0 +1,22 @@
+angular.module('myApp.factory', [])
+    .factory('courseStudents', function(){
+        var courseStudents = {};
+        courseStudents.students = [
+            "Attila",
+            "Peter",
+            "Renato",
+            "Richard"
+        ];
+
+        return courseStudents.students;
+    })
+    .factory('courseLessons', function(){
+        var courseLessons = {};
+        courseLessons.lessons = [
+            "JavaScript",
+            "C++",
+            "Python"
+        ];
+
+        return courseLessons.lessons;
+    });
